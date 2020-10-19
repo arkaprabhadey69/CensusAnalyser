2c04f85d97666d78a68aaf9a622a3fbbdb4ca802
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-19 11:42:52 +0530

    [Arka]Added: Test case to check the number of entries in IndianStateCode file

--- src/test/java/com/bl/census/CensusAnalyserTest.java
+++ src/test/java/com/bl/census/CensusAnalyserTest.java
@@ -11,6 +11,7 @@ public class CensusAnalyserTest {
     private static final String WRONG_CSV_FILE_TYPE_PATH = "./src/test/resources/IndiaStateCensusData.txt";
     private static final String WRONG_FILE_TYPE_PATH = "./src/test/resources/IndianCensus.csv";
     private static final String WRONG_FILE_HEADER_PATH = "./src/test/resources/IndianNewCensus.csv";
+    private static final String INDIA_STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -64,6 +65,17 @@ public class CensusAnalyserTest {
             censusAnalyser.loadIndiaCensusData(WRONG_FILE_HEADER_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+    @Test
+
+    public void givenStatesReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaStateData(INDIA_STATE_CODE_CSV_FILE_PATH);
+            Assert.assertEquals(37,numOfRecords);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
         }
     }
 
