f83485497a3565f14a4a33cb3ec661bda45d3d66
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-19 11:05:39 +0530

    [Arka]Added:Sad test case with wrong file name to check whether it is throwing exception

--- src/test/java/com/bl/census/CensusAnalyserTest.java
+++ src/test/java/com/bl/census/CensusAnalyserTest.java
@@ -7,6 +7,7 @@ import org.junit.rules.ExpectedException;
 public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -15,6 +16,17 @@ public class CensusAnalyserTest {
             Assert.assertEquals(29, numOfRecords);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenIndiaCensusDataWithWrongFileShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
     }
 
