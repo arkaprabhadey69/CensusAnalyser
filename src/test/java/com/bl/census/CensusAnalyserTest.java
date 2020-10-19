e7af9a804da33a12b28c020d4fac191c224a4312
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-19 11:02:24 +0530

    [Arka]Added:Test case to validate number of entries

--- src/test/java/com/bl/census/CensusAnalyserTest.java
+++ src/test/java/com/bl/census/CensusAnalyserTest.java
@@ -7,9 +7,6 @@ import org.junit.rules.ExpectedException;
 public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
-    private static final String WRONG_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.txt";
-    private static final String INDIA_STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
-
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
